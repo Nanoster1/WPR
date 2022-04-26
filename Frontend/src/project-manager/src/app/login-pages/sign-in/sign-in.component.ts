cd44b6eecfcd1bb3fed07d55b2ef94c593f6a50e
Slava <sh-e-95@mail.ru>
2022-04-26 17:35:54 +0500

    create login template

--- Frontend/src/project-manager/src/app/login-pages/sign-in/sign-in.component.ts
+++ Frontend/src/project-manager/src/app/login-pages/sign-in/sign-in.component.ts
@@ -3,7 +3,7 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.sass']
+  styleUrls: ['./sign-in.component.sass','../login.sass']
 })
 export class SignInComponent {
 
