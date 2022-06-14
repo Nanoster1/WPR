7f660458f037a1cb6a6df929e66202da42848b9e
Slava <sh-e-95@mail.ru>
2022-06-15 00:37:26 +0500

    create visual

--- Frontend/src/project-manager/src/app/login-pages/sign-in/sign-in.component.ts
+++ Frontend/src/project-manager/src/app/login-pages/sign-in/sign-in.component.ts
@@ -1,10 +1,27 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {delay} from "../../../services/timeServices";
 
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.sass','../login.sass']
+  styleUrls: ['./sign-in.component.sass', '../login.sass']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  text: string = ""
+
+  ngOnInit(): void {
+    const title = document.getElementById('#title');
+    const secretText = "Welcome to WPR!\nHere we go are again"
+
+    secretText.split('').forEach(
+      (x, index) => {
+        delay(100 * index, () => {
+          if (title != null)
+            this.text += x;
+        })
+      }
+    )
+  }
+
 
 }
